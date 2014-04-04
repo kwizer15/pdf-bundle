89460843837cb80e551413fb94f29b116d953257
Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
2014-04-04 23:07:51 +0200

    Fixed namespace FPDFBuilder

---
+++ Tests/FPDF/FPDFBuilderTest.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is part of the bundle package.
+ *
+ * (c) Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Kwizer\PdfBundle\Tests\FPDF;
+
+/**
+ * @author Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
+ */
+class classname
+{
+	
+}
