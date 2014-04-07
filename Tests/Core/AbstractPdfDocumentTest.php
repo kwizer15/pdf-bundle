14a575ce7be3c8789f2e42b90c18c9624f73538e
Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
2014-04-07 09:55:07 +0200

    Create AbstractPdfDocumentTest (no test)

---
+++ Tests/Core/AbstractPdfDocumentTest.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the KwizerPdfBundle package.
+*
+* (c) Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace Kwizer\PdfBundle\Tests\Core;
+
+/**
+ * @author Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
+ */
+class AbstractPdfDocumentTest implements \PHPUnit_Framework_TestCase
+{
+	public function testTrue()
+	{
+		$this->assertTrue(true);
+	}
+}
