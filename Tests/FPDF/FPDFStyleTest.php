712327998495b742483844ad6f1a0c0986301b55
Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
2014-04-08 13:56:45 +0200

    FPDFStyleTest

---
+++ Tests/FPDF/FPDFStyleTest.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the bundle package.
+*
+* (c) Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace Kwizer\PdfBundle\Tests\FPDF;
+
+use Kwizer\PdfBundle\FPDF\FPDFStyle;
+
+/**
+ * @author Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
+ */
+class FPDFStyleTest extends \PHPUnit_Framework_TestCase
+{
+	public function testTrue()
+	{
+		$this->markTestIncomplete('Ce test n\'a pas encore été implémenté.');
+	}
+}
