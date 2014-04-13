9bc6a48379255b97a9748c1e37177b33559f7d5c
Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
2014-04-13 17:17:05 +0200

    Delete twig.xml (doesn't exist)

--- DependencyInjection/KwizerPdfExtension.php
+++ DependencyInjection/KwizerPdfExtension.php
@@ -24,6 +24,5 @@ class KwizerPdfExtension extends Extension
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
-        $loader->load('twig.xml');
     }
 }
